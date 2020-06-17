c676f3daa2f92f3de7d60efbecb2785df80e05b6
Léonard Bise <leonard.bise@gmail.com>
2020-06-17 13:06:53 +0200

    toolbox: Add accept function

--- toolbox_app/src/main.c
+++ toolbox_app/src/main.c
@@ -10,6 +10,7 @@ LOG_MODULE_REGISTER(toolbox, LOG_LEVEL_DBG);
 #include <shell/shell.h>
 #include <net/socket.h>
 #include <net/tls_credentials.h>
+#include "net_private.h"
 
 /* Used signed certificates */
 #define SIGNED_CERTS
@@ -159,6 +160,43 @@ static int cmd_sock_listen(const struct shell *shell, size_t argc, char *argv[])
 	return 0;
 }
 
+static int cmd_sock_accept(const struct shell *shell, size_t argc, char *argv[])
+{
+	/* sock accept <fd> */
+	int arg = 1;
+	int fd;
+	struct sockaddr client_addr;
+	socklen_t client_addr_len = sizeof(client_addr);
+	int err;
+
+	if (argc < 2) {
+		return -ENOEXEC;
+	}
+
+	fd = strtoul(argv[arg++], NULL, 10);
+
+	err = accept(fd, &client_addr, &client_addr_len);
+	if (err < 0) {
+		LOG_ERR("Cannot accept client (%d)", -errno);
+		return -ENOEXEC;
+	}
+
+	if (client_addr.sa_family == AF_INET) {
+		LOG_INF("New client fd=%d %s:%d", err,
+			log_strdup(net_sprint_ipv4_addr(
+					&net_sin(&client_addr)->sin_addr)),
+			ntohs(net_sin(&client_addr)->sin_port));
+	} else if (client_addr.sa_family == AF_INET6) {
+		LOG_INF("New client fd=%d [%s]:%d", err,
+			log_strdup(net_sprint_ipv6_addr(
+					&net_sin6(&client_addr)->sin6_addr)),
+				   ntohs(net_sin6(&client_addr)->sin6_port));
+	}
+
+	return 0;
+}
+
+
 static int cmd_sock_close(const struct shell *shell, size_t argc, char *argv[])
 {
 	/* sock close <fd> */
@@ -242,19 +280,22 @@ SHELL_STATIC_SUBCMD_SET_CREATE(sock_commands,
 	SHELL_CMD(new, NULL, "'sock new <family inet|inet6> <type stream|dgram>"
 			     "<proto tcp|tls_1_2>' Create a new socket",
 		  cmd_sock_new),
-	SHELL_CMD(connect, NULL, "'sock connect <fd> <ip>:<port> or [<ipv6>]:<port>'"
+	SHELL_CMD(connect, NULL, "'sock connect <fd> <ip>:<port> or [<ipv6>]:<port>' "
 			     "Connect a socket",
 		  cmd_sock_connect),
-	SHELL_CMD(bind, NULL, "'sock bind <fd> <ip>:<port> or [<ipv6>]:<port>'"
+	SHELL_CMD(bind, NULL, "'sock bind <fd> <ip>:<port> or [<ipv6>]:<port>' "
 			     "Connect a socket",
 		  cmd_sock_bind),
-	SHELL_CMD(listen, NULL, "'sock listen <fd> <max client>'"
+	SHELL_CMD(listen, NULL, "'sock listen <fd> <max client>' "
 			     "Make a socket listen",
 		  cmd_sock_listen),
-	SHELL_CMD(close, NULL, "'sock close <fd>'"
+	SHELL_CMD(accept, NULL, "'sock accept <fd>' "
+			     "Accept an incoming connection",
+		  cmd_sock_accept),
+	SHELL_CMD(close, NULL, "'sock close <fd>' "
 			     "Close a socket",
 		  cmd_sock_close),
-	SHELL_CMD(tlsopt, NULL, "'sock tlsopt <fd> <srv|cli>'"
+	SHELL_CMD(tlsopt, NULL, "'sock tlsopt <fd> <srv|cli>' "
 			     "Set TLS related options",
 		  cmd_sock_tlsopt),
 
